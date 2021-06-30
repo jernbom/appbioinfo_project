937ee5b32c6621b84f215971ade334dce8a64e53
jernbom <august.jernbom@scilifelab.se>
2021-06-30 13:14:05 +0200

    Force gunzip overwrite

--- string_dl.sh
+++ string_dl.sh
@@ -1,5 +1,5 @@
 # Download STRING H.Sapiens data
 curl -o downloads/STRING_h_sapiens.txt.gz https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
-gunzip downloads/STRING_h_sapiens.txt.gz
+gunzip -f downloads/STRING_h_sapiens.txt.gz
 # Download protein domain data
 curl -L -o downloads/Ensembl_protein_domain.txt https://stockholmuniversity.box.com/shared/static/n8l0l1b3tg32wrzg2ensg8dnt7oua8ex
